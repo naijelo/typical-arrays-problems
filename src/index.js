5817bf0b6f386e80c509160095f8501421617f07
naijel <naijel.a@gmail.com>
2020-02-27 20:20:25 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
+  if (array == undefined || array.length == 0) {
   return 0;
+  } else {
+    return array.sort(function(a, b) { return a - b; })[0]
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    return array.sort(function(a, b) { return b - a; })[0];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+   } else {
+  let sum = 0;
+  let len = array.length;
+  for (let i = 0; i < len; i++) {
+    sum += array[i];
+}
+  return (sum / len);
+}
 }
